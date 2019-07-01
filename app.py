8223bd06d1b0a8127ce813a2679731b3da8d1457
Aji Kamaludin <aji19kamaludin@gmail.com>
2019-07-01 14:06:14 +0700

    add bm25 algorithm

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import pprint
 from tfidf import TfIdf
 from widf import WIdf
 from tfrf import TFRF
+from bm25 import BM25
 
 document = [
     'Shipment of gold damaged in a fire',
@@ -12,22 +13,40 @@ document = [
 #     'Shipment of gold damaged in a fire Delivery of silver arrived in a silver truck Shipment of gold arrived in a truck truck truck truck'
 # ]
 
-q = 'silver'
+q = 'gold'
 
+dictOf = { i : document[i] for i in range(0, len(document) ) }
+
+print('+----------------------------------+')
+print('documents : ')
+pprint.pprint(dictOf)
+print('')
+print('query :' + q)
+print('+----------------------------------+')
+
+print('')
 print('Pembobotan TF-IDF')
 tfidf = TfIdf().transform(q=q, document=document)
 print("Bobot rata-rata: " + str(tfidf.weight_average()))
 pprint.pprint(tfidf.get_weight())
 print("+---------------------------------+")
 
-print('Pembobotan W-IDF : ' + q)
+print('')
+print('Pembobotan W-IDF')
 widf = WIdf().transform(q=q, document=document)
 print("Bobot rata-rata: " + str(widf.weight_average()))
 pprint.pprint(widf.get_weight())
 print("+---------------------------------+")
 
-print('Pembobotan TFRF : ' + q)
+print('')
+print('Pembobotan TFRF')
 tfrf = TFRF().transform(q=q,document=document)
+print("Bobot rata-rata: " + str(tfrf.weight_average()))
 pprint.pprint(tfrf.get_weight())
-print(tfrf.weight_average())
+print("+---------------------------------+")
+
+print('')
+print('Pembobotan BM25')
+bm25 = BM25().transform(q=q, document=document)
+pprint.pprint(bm25.weigth())
 print("+---------------------------------+")
---
+++ bm25.py
@@ -0,0 +1,46 @@
+import pprint
+import math
+
+class BM25():
+
+    k1 = 1.2
+    b = 0.75
+
+    def __init__(self):
+        self.query = ''
+        self.document = []
+        self.corpus = {}
+        self.lave = 0
+        self.document_contains = 0
+
+    def transform(self, q, document):
+        self.query = q
+        self.document = document
+        for index, item in enumerate(self.document):
+            words = item.split(' ')
+            tf = 0
+            for word in words:
+                if(self.query.lower() == word.lower()):
+                    tf += 1
+            self.corpus[index] = {"tf" : tf, "ldi" : len(words)}
+            if(tf != 0):
+                self.document_contains += 1
+
+            self.lave += len(words)
+        self.lave = self.lave / len(document)
+        return self
+
+    def K(self, ldi):
+        return self.k1 * ((1 - self.b) + ( self.b * ( ldi / self.lave ) ))
+
+    def idf(self):
+        return math.log(len(self.document) / self.document_contains ,10)
+
+    def weigth(self):
+        for key, value in self.corpus.items():
+            if(value['tf'] > 0):
+                weight = self.idf() * ( ( ( self.k1 + 1 ) * value['tf'] ) / (self.K(value['ldi']) + value['tf']) )
+            else:
+                weight = 0
+            self.corpus[key]["weight"] = weight
+        return self.corpus
